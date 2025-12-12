3a5e3ad4e5fb48e8861dea5909cfb98be3efac85
bethe-elias <betineba1@gmail.com>
2025-12-13 00:21:42 +0300

    building payment page UI

--- amazon-replica-project/src/Pages/Payment/Payment.jsx
+++ amazon-replica-project/src/Pages/Payment/Payment.jsx
@@ -1,10 +1,94 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Layout from '../../Components/Layout/Layout'
+import classes from './payment.module.css'
+import { DataContext } from '../../Components/DataProvider/DataProvider';
+import ProductCard from '../../Components/Product/ProductCard';
+import { useElements, useStripe,CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from '../../Components/CurrencyFormat/CurrencyFormat';
 
 function Payment() {
+ 
+
+const [{basket,user}] = useContext(DataContext)
+
+// for total items
+const totalItem = basket?.reduce((amount, item) => {
+  return item.amount + amount;
+}, 0);
+//  for price
+const total = basket.reduce((amount, item) => {
+  return item.price * item.amount + amount;
+}, 0);
+
+const [cardError,setCardError] = useState(null)
+
+const stripe = useStripe();
+const elements = useElements();
+
+
+ const handleChange = (e) => {
+  console.log(e);
+  e?.error?.message ? setCardError(e?.error?.message) : setCardError("")
+ }
+
+ const handlePayment = (e) => {
+  e.preventDefault()
+
+  // backend function - get client secret
+
+  // client side/react side confirmation
+
+  // after confiramtion order firestore database,save and clear basket
+
+ }
+
   return (
     <Layout>
-      <div>Payment</div>
+      <div className={classes.payment_header}>checkout({totalItem}) items</div>
+      <section className={classes.payment}>
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>{user?.email}</div>
+            <div>123 React Lane</div>
+            <div>Ethiopia</div>
+          </div>
+        </div>
+        <hr />
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+            {basket?.map((item, i) => (
+              <ProductCard product={item} key={i} flex={true} />
+            ))}
+          </div>
+        </div>
+        <hr />
+        <div className={classes.flex}>
+          <h3>Payment methods</h3>
+          <div className={classes.payment_card_container}>
+            <div className={classes.payment_details}>
+              <form action="" onSubmit={handlePayment}>
+                {/* error */}
+                {cardError && (
+                  <small style={{ color: "red" }}>{cardError}</small>
+                )}
+                {/* card */}
+                <CardElement onChange={handleChange} />
+                {/* price to pay */}
+                <div className={classes.payment_price}>
+                  <div>
+                    <span style={{display:"flex" ,gap:"5px"}}>
+                      <p>Total order  |</p>  <CurrencyFormat amount={total} />
+                    </span>
+                  </div>
+                  <button>Pay now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
--- amazon-replica-project/src/Router.jsx
+++ amazon-replica-project/src/Router.jsx
@@ -7,6 +7,13 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results"
 import ProductDetails from './Pages/ProductDetails/ProductDetails'
+import {Elements} from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js';
+
+
+const stripePromise = loadStripe(
+  "pk_test_51ScMic1IP2skISsbbyVujSJQb629s4rOVpn3LADdU8FSuCY9OLCjxHrtEXDi1cVxDoHwhzCUubkbiK5ZHW1n6Z9300m0B7h3nw"
+);
 
 function Routing() {
   return (
@@ -14,7 +21,14 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
