8fec6d6e694297feaa805e398375cd5a45bc2d83
bethe-elias <betineba1@gmail.com>
2025-12-29 19:11:58 +0300

    firebase api

--- amazon-replica-project/src/Api/axios.js
+++ amazon-replica-project/src/Api/axios.js
@@ -7,11 +7,11 @@ const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-a73d3/us-central1/api",
 
   //firebase.com deployed url-backend(functions with card)
-  // baseURL: "https://us-central1-clone-a73d3.cloudfunctions.net/api",
+  baseURL: "https://us-central1-clone-a73d3.cloudfunctions.net/api",
 
 
   //backend deployed url for amazon api with out card on render.com
-  baseURL: "https://amazon-api-deploy-2-2ih7.onrender.com/",
+  // baseURL: "https://amazon-api-deploy-2-2ih7.onrender.com/",
 });
 
 export {axiosInstance}
