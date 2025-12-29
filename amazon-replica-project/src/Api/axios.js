6558f6a7d2a23dee740007da5e8a0ec90fd58ce8
bethe-elias <betineba1@gmail.com>
2025-12-29 19:38:07 +0300

    Fix Vite entry: move index.html to root

--- amazon-replica-project/src/Api/axios.js
+++ amazon-replica-project/src/Api/axios.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   //local url
-  // baseURL: "http://127.0.0.1:5001/clone-a73d3/us-central1/api",
+  baseURL: "http://127.0.0.1:5001/clone-a73d3/us-central1/api",
 
   //firebase.com deployed url-backend(functions with card)
-  baseURL: "https://us-central1-clone-a73d3.cloudfunctions.net/api",
+  // baseURL: "https://us-central1-clone-a73d3.cloudfunctions.net/api",
 
 
   //backend deployed url for amazon api with out card on render.com
